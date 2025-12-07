684a7201d8d6c67de41074473494ac857a26bedc
Vadym Novoselskyi <vadno575@student.liu.se>
2025-12-08 00:15:25 +0100

    obstIcle -> obstAcle

--- src/Obstacle.h
+++ src/Obstacle.h
@@ -1,16 +1,16 @@
-#ifndef OBSTICLE_H
-#define OBSTICLE_H
+#ifndef OBSTACLE_H
+#define OBSTACLE_H
 
 #include <SFML/Graphics.hpp>
 #include <cmath>
 
 #include "Entity.h"
 
-class Obsticle : public Entity
+class Obstacle : public Entity
 {
   public:
     // Entity(std::string const &tag, sf::Vector2f const &pos);
-    Obsticle(sf::Texture texture, sf::Vector2f position);
+    Obstacle(sf::Texture texture, sf::Vector2f position);
 
     void onCollision(std::string const &other) override;
     void move() override;
