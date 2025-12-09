a19523a633544472c963b7b3d12525af3f295aba
Vincent Ivarsson <viniv321@student-liu.se>
2025-12-09 17:58:21 +0100

    slut för idag ända i projectile.cc

--- src/Projectile.cc
+++ src/Projectile.cc
@@ -19,40 +19,8 @@ void Projectile::onCollision(std::string const &other)
 
 void Projectile::move()
 {
-    // sf::Sprite::Transformable::move(-std::cos(Transformable::getRotation()) * velocity, -std::sin(Transformable::getRotation()) * velocity);
     sf::Vector2f dir {std::cos((sf::Sprite::getRotation() + 90) * M_PI / 180.0f), std::sin((sf::Sprite::getRotation() + 90)  * M_PI / 180.0f)}; 
-    // if(Transformable::getRotation() == 0)
-    // {
-    //     dir = {0, 1};
-    // }
-    // else if (Transformable::getRotation() == 45)
-    // {
-    //     dir = {-0.5f, 0.5f};
-    // }
-    // else if(Transformable::getRotation() == 90)
-    // {
-    //     dir = {-1, 0};
-    // }
-    // else if(Transformable::getRotation() == 135)
-    // {
-    //     dir = {-0.5f, -0.5f};
-    // }
-    // else if(Transformable::getRotation() == 180)
-    // {
-    //     dir = {0, -1};
-    // }
-    // else if(Transformable::getRotation() == 225)
-    // {
-    //     dir = {0.5f, -0.5f};
-    // }
-    // else if(Transformable::getRotation() == 270)
-    // {
-    //     dir = {1, 0};
-    // }
-    // else if(Transformable::getRotation() == 315)
-    // {
-    //     dir = {0.5f, 0.5f};
-    // }
+
     sf::Sprite::Transformable::move(-dir.x * velocity, -dir.y * velocity);
 }
 
