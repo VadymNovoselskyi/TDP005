e8d6a68f134c197527d2f13fc1ab9fd9408e75a3
Vincent Ivarsson <viniv321@student-liu.se>
2025-12-09 17:45:00 +0100

    spelare kan sjukta i olika riktningar

--- src/Projectile.cc
+++ src/Projectile.cc
@@ -6,6 +6,7 @@ Projectile::Projectile(sf::Vector2f const &pos, double rotation, double velocity
 {
     sf::Sprite::setTexture(*texture);
     sf::Sprite::setRotation(rotation);
+    sf::Sprite::setOrigin(sf::Sprite::getScale().x / 2, sf::Sprite::getScale().y / 2);
 }
 
 void Projectile::onCollision(std::string const &other)
@@ -18,8 +19,40 @@ void Projectile::onCollision(std::string const &other)
 
 void Projectile::move()
 {
-    sf::Vector2f dir {std::cos(Transformable::getRotation()), std::sin(Transformable::getRotation())};
-
+    // sf::Sprite::Transformable::move(-std::cos(Transformable::getRotation()) * velocity, -std::sin(Transformable::getRotation()) * velocity);
+    sf::Vector2f dir {std::cos((sf::Sprite::getRotation() + 90) * M_PI / 180.0f), std::sin((sf::Sprite::getRotation() + 90)  * M_PI / 180.0f)}; 
+    // if(Transformable::getRotation() == 0)
+    // {
+    //     dir = {0, 1};
+    // }
+    // else if (Transformable::getRotation() == 45)
+    // {
+    //     dir = {-0.5f, 0.5f};
+    // }
+    // else if(Transformable::getRotation() == 90)
+    // {
+    //     dir = {-1, 0};
+    // }
+    // else if(Transformable::getRotation() == 135)
+    // {
+    //     dir = {-0.5f, -0.5f};
+    // }
+    // else if(Transformable::getRotation() == 180)
+    // {
+    //     dir = {0, -1};
+    // }
+    // else if(Transformable::getRotation() == 225)
+    // {
+    //     dir = {0.5f, -0.5f};
+    // }
+    // else if(Transformable::getRotation() == 270)
+    // {
+    //     dir = {1, 0};
+    // }
+    // else if(Transformable::getRotation() == 315)
+    // {
+    //     dir = {0.5f, 0.5f};
+    // }
     sf::Sprite::Transformable::move(-dir.x * velocity, -dir.y * velocity);
 }
 
