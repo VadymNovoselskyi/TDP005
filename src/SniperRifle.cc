39eb46c3f5e998ffaa55a01d5b85102de7a08ee1
Vincent Ivarsson <viniv321@student-liu.se>
2025-12-11 14:36:10 +0100

    har inte lyckats få snipern att sjukta dit den ska men gör denna push så de går att merge

--- src/SniperRifle.cc
+++ src/SniperRifle.cc
@@ -20,10 +20,10 @@ void SniperRifle::shoot()
 {
     if (Entity *closeEnemy = Map::instance()->getClosestEnemy(); closeEnemy != nullptr)
     {
-        calculateRotation(closeEnemy);
+        //calculateRotation(closeEnemy);
 
         Map::instance()->addEntity(new Projectile{Transformable::getPosition(),
-                                                  bulletRotation,
+                                                  /*bulletRotation*/ Transformable::getRotation(),
                                                   speed,
                                                   damage * damageMultiplication,
                                                   bulletHP,
@@ -35,10 +35,10 @@ void SniperRifle::calculateRotation(Entity *closeEnemy)
 {
     // detta är taget från classen player fuktionen updateRotation
     double rotationRadians = std::atan2((closeEnemy->getPosition().x - Transformable::getPosition().x), closeEnemy->getPosition().y - Transformable::getPosition().y);
-    double rotation = rotationRadians * (180 / M_PI); // transform radians to rotation
+    double rotation = rotationRadians ; // transform radians to rotation
     
     std::cout << "enemy "<< closeEnemy->getPosition().x << " | "<< closeEnemy->getPosition().y << std::endl;
-    std::cout << "weapon "<< Transformable::getPosition().x << " | "<< Transformable::getPosition().y << std::endl;
+    std::cout << "weapon "<< closeEnemy->getPosition().x << " | "<< Transformable::getPosition().y << std::endl;
     bulletRotation = rotation;
     std::cout << rotation << " | "<< rotationRadians << std::endl;
 }
