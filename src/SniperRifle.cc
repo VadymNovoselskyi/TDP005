2f5550e6cf78e4e7c54fe926ad39828a3581363b
Vincent Ivarsson <viniv321@student-liu.se>
2025-12-16 13:06:09 +0100

    försökte få snipern att funka som den ska men lyckas inte ska greja med projectile lite nu (har varit sjuk)

--- src/SniperRifle.cc
+++ src/SniperRifle.cc
@@ -20,22 +20,22 @@ void SniperRifle::shoot()
 {
     if (Entity *closeEnemy = Map::instance()->getClosestEnemy(); closeEnemy != nullptr)
     {
-        //calculateRotation(closeEnemy);
+        // calculateRotation(closeEnemy);
+    }
 
-        Map::instance()->addEntity(new Projectile{Transformable::getPosition(),
+    Map::instance()->addEntity(new Projectile{Transformable::getPosition(),
                                                   /*bulletRotation*/ Transformable::getRotation(),
                                                   speed,
                                                   damage * damageMultiplication,
                                                   bulletHP,
                                                   "Sniper_bullet.png"});
-    }
 }
 
 void SniperRifle::calculateRotation(Entity *closeEnemy)
 {
     // detta är taget från classen player fuktionen updateRotation
-    double rotationRadians = std::atan2((closeEnemy->getPosition().x - Transformable::getPosition().x), closeEnemy->getPosition().y - Transformable::getPosition().y);
-    double rotation = rotationRadians ; // transform radians to rotation
+    double rotationRadians = std::atan2(Transformable::getPosition().x - closeEnemy->getPosition().x, Transformable::getPosition().y - closeEnemy->getPosition().y);
+    double rotation = rotationRadians * (180 / M_PI); // transform radians to rotation
     
     std::cout << "enemy "<< closeEnemy->getPosition().x << " | "<< closeEnemy->getPosition().y << std::endl;
     std::cout << "weapon "<< closeEnemy->getPosition().x << " | "<< Transformable::getPosition().y << std::endl;
