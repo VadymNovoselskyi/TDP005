6b4a4a08c17adeb34ae487e77ea31f22f59ae6f8
Tom Glimberg <tomgl406@student.liu.se>
2025-12-18 01:58:40 +0000

    cometerat i EnemyProjectile.h

--- src/EnemyProjectile.h
+++ src/EnemyProjectile.h
@@ -3,12 +3,38 @@
 
 #include "Entity.h"
 #include "Projectile.h"
+/**
+ * EnemyProjectile
+ *
+ * EnemyProjectile is responsible for:
+ * - hitting player and interact with them
+ */
 
 class EnemyProjectile : public Projectile
 {
   public:
-    EnemyProjectile(sf::Vector2f const &pos, double rotation, double velocity, double damage, double hp, std::string pngName);
+   /**
+     * Construct a EnemyProjectile that send pos, rotation, velocity, damage, bulletHP and pngName 
+     * to projektile construct
+     *
+     * @param pos Position is so it and other know where it is
+     * @param rotation is for the sprite so its rotated
+     * @param velocity is the projectile speed and make how fast the projectile travel
+     * @param damage is how how mutch enemys hit points should drop when hit
+     * @param bulletHP is the hit points the bullet has
+     * @param pngName is the name of the png
+     */
 
+
+     */
+
+    EnemyProjectile(sf::Vector2f const &pos, double rotation, double velocity, double damage, double hp, std::string pngName);
+    
+    /**
+     * Handel collison white a player
+     *
+     * @param other a pointer to a Entity so the can interakt with itch other
+     */
     void onCollision(Entity *other) override;
 
   private:
